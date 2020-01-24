1b9c5dc5f981e18a7c3c97879c35045bbf9322cf
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-24 12:45:27 -0800

    Update CharacterInput.java

--- CharacterInput.java
+++ CharacterInput.java
@@ -1,8 +1,12 @@
-//C:\Program Files\Java\jdk-13.0.2\bin>java.exe C:\Users\cgarrido\Desktop\lab.java
+//C:\Program Files\Java\jdk-13.0.2\bin>java.exe C:\Users\cgarrido\Desktop\CharacterInput.java
+import java.util.Scanner;
 public class CharacterInput {
 	public static void main(String[] args){
-		String name = "Charles";
-		int age = 25;
+		System.out.println("What is your name?");
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+		System.out.println("Okay " + name + " how old are you?");
+		int age = input.nextInt();
 		int birth = 2020 - age;
 		int lifespan = birth + 100;
 		System.out.println("Hello " + name + ". You will turn one hundred years old in the year " + lifespan);
