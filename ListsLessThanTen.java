ab8d4fcd71ef8b39073457f4142d1914206112b8
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-24 13:11:24 -0800

    Create ListsLessThanTen.java

---
+++ ListsLessThanTen.java
@@ -0,0 +1,18 @@
+//C:\Program Files\Java\jdk-13.0.2\bin>java.exe C:\Users\cgarrido\Desktop\ListsLessThanTen.java
+/*
+Take a list, say for example this one:
+	a = [1, 1, 2, 3, 5, 8, 13, 21, 32, 55, 89]
+and write a program that prints out all the elements of the list that are less than 5. 
+*/
+class ListsLessThanTen{
+	public static void main(String args[]){
+		//Start coding here
+		int[] lista = {1, 1, 2, 3, 5, 8, 13, 21, 32, 55, 89};
+		int[] listb;
+		for (int iterate : lista){ //There's an issue here 
+			if (int iterate : lista < 5){ //There's also an issue here 
+				listb.push(x) //And one final one here 
+			}
+		}
+	}
+}
