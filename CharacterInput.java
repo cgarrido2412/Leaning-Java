bf02a07c6b93ae844e7c37908908c9b5aa61d21c
cgarrido2412 <54080405+cgarrido2412@users.noreply.github.com>
2020-01-24 12:05:53 -0800

    Create CharacterInput.java

---
+++ CharacterInput.java
@@ -0,0 +1,10 @@
+//C:\Program Files\Java\jdk-13.0.2\bin>java.exe C:\Users\cgarrido\Desktop\lab.java
+public class CharacterInput {
+	public static void main(String[] args){
+		String name = "Charles";
+		int age = 25;
+		int birth = 2020 - age;
+		int lifespan = birth + 100;
+		System.out.println("Hello " + name + ". You will turn one hundred years old in the year " + lifespan);
+	}
+}
